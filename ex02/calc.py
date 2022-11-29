9fbf4544860019e01bfa5fcbdeb63064051e6f46
KotaOkuda <c0a2102727@edu.teu.ac.jp>
2022-11-29 15:45:03 +0900

    エラーと数字入れ替え

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,14 +1,16 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def math(siki):
+def math(siki):                 #計算結果を出力する関数
     entry.delete(0, tk.END)
-    if(type(eval(siki)) is int):
+
+    #エラー表示
+    try:
         entry.insert(tk.END, str(eval(siki)))
-    else:
+    except NameError:
         entry.insert(tk.END, "エラー")
 
-def button_click(event):
+def button_click(event):        #キーが押されたときの関数
     btn = event.widget
     num = btn["text"]
     #tkm.showinfo("", f"{num}ボタンがクリックされました")
@@ -28,6 +30,15 @@ entry.grid(row = 0, column = 0, columnspan = 3)
 r, c = 1, 0
 
 for num in range(9, -1, -1):
+
+    #数字の場所入れ替え
+    if num == 0:
+        None
+    elif num % 3 == 0:
+        num -= 2
+    elif num % 3 == 1:
+        num += 2
+
     button = tk.Button(root, text = f"{num}", width = 4, height = 2, font=("", 30))
     button.grid(row = r, column = c)
     button.bind("<1>", button_click)
