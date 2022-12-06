7fcab23e4a2aa445668853195c0cf1efe42b7e87
KotaOkuda <c0a2102727@edu.teu.ac.jp>
2022-12-06 14:33:46 +0900

    問題6

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,10 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key = ""
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -16,6 +20,7 @@ if __name__ == "__main__":
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image = kokaton, tag = "kokaton")
 
-    root.bind("<KeyPress", key_down)
+    root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
 
     root.mainloop()
