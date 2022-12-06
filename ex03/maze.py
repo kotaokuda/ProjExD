a06d2832bd8828aa8daa58bb765b18f7f0c918d0
KotaOkuda <c0a2102727@edu.teu.ac.jp>
2022-12-06 18:25:41 +0900

    クリア確認のif文をまとめた #7

--- ex03/maze.py
+++ ex03/maze.py
@@ -14,15 +14,15 @@ def main_proc():
     if key == "Up" and maze_list[mx][my - 1] == 0:      #上が入力されかつ、移動先が道なら
         my -= 1
     if key == "Down" and maze_list[mx][my + 1] == 0:        #下が入力されかつ、移動先が道なら
-        my += 1
-        if mx == 13 and my == 7:
-            finish()                
+        my += 1           
     if key == "Left" and maze_list[mx - 1][my] == 0:        #左が入力されかつ、移動先が道なら
         mx -= 1
     if key == "Right" and maze_list[mx + 1][my] == 0:       #右が入力され、移動先が道なら
         mx += 1
-        if mx == 13 and my == 7:
-            finish()
+
+    #ゴールに到着したかの確認
+    if mx == 13 and my == 7:
+        finish()
 
     #rを押すとリセット
     if key == "r":
@@ -56,15 +56,14 @@ if __name__ == "__main__":
     setumei.place(x = 0, y = 0)
 
     maze_list = maze_maker.make_maze(15, 9)
-    maze_maker.show_maze(canvas, maze_list)    
-
-    key = ""
+    maze_maker.show_maze(canvas, maze_list)
 
     kokaton = tk.PhotoImage(file = "fig/0.png")
     mx, my = 1, 1
     cx, cy = mx * 100 + 50, my * 100 + 50
     canvas.create_image(cx, cy, image = kokaton, tag = "kokaton")
 
+    key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     main_proc()
