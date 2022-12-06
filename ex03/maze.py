b7bc97175284df0c524d11ab31c1b3772907a75f
KotaOkuda <c0a2102727@edu.teu.ac.jp>
2022-12-06 14:29:34 +0900

    問題4

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
     canvas = tk.Canvas(root, width = 1500, height = 900, bg = "black")
     canvas.pack()
 
-    kokaton = tk.PhotoImage(file = "fig/8.png")
+    key = ""
+
+    kokaton = tk.PhotoImage(file = "fig/0.png")
     cx, cy = 300, 400
     canvas.create_image(cx, cy, image = kokaton, tag = "kokaton")
 
