d42e20c8e96cad09461facec8e899e39411048ed
KotaOkuda <c0a2102727@edu.teu.ac.jp>
2022-12-20 14:55:30 +0900

    問題3

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -15,12 +15,55 @@ class Screen:
         self.sfc.blit(self.bgi_sfc, self.bgi_rct)
 
 
-key_delta = {
-    pg.K_UP:    [0, -1],
-    pg.K_DOWN:  [0, +1],
-    pg.K_LEFT:  [-1, 0],
-    pg.K_RIGHT: [+1, 0],
-}
+class Bird:
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],
+    }
+
+    def __init__(self, img_path, ratio, xy):
+        self.sfc = pg.image.load(img_path)      #"fig/6.png"
+        self.sfc = pg.transform.rotozoom(self.sfc, 0, ratio)      #tori_sfc, 0, 2.0
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy      #900, 400
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        key_dct = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items():
+            if key_dct[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]        
+            if check_bound(self.rct, scr.rct) != (+1, +1):
+                self.rct.centerx -= delta[0]
+                self.rct.centery -= delta[1]
+        self.blit(scr)
+
+
+class Bomb:
+    def __init__(self, clo, herh, v, scr:Screen):
+            self.sfc = pg.Surface((2 * herh, 2 * herh)) # 正方形の空のSurface
+            self.sfc.set_colorkey((0, 0, 0))
+            pg.draw.circle(self.sfc, clo, (herh, herh), herh)
+            self.rct = self.sfc.get_rect()
+            self.rct.centerx = random.randint(0, scr.rct.width)
+            self.rct.centery = random.randint(0, scr.rct.height)
+            self.vx = v[0]
+            self.vy = v[1]
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+    
+    def update(self, scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        scr.sfc.blit(self.sfc, self.rct)
 
 
 def check_bound(obj_rct, scr_rct):
@@ -40,55 +83,28 @@ def check_bound(obj_rct, scr_rct):
 def main():
     clock =pg.time.Clock()
 
-    #練習1
     scr = Screen("逃げろ!こうかとん", (1600, 900), "fig/pg_bg.jpg")
 
-    # 練習３
-    tori_sfc = pg.image.load("fig/6.png")
-    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 2.0)
-    tori_rct = tori_sfc.get_rect()
-    tori_rct.center = 900, 400
-    # scrn_sfcにtori_rctに従って，tori_sfcを貼り付ける 
-    scr.brit()
+    kkt = Bird("fig/6.png", 2.0, (900, 400))
+    kkt.update(scr)
 
-    # 練習５
-    bomb_sfc = pg.Surface((20, 20)) # 正方形の空のSurface
-    bomb_sfc.set_colorkey((0, 0, 0))
-    pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10)
-    bomb_rct = bomb_sfc.get_rect()
-    bomb_rct.centerx = random.randint(0, scrn_rct.width)
-    bomb_rct.centery = random.randint(0, scrn_rct.height)
-    scr.brit()
-    vx, vy = +1, +1
+    bkd = Bomb((255, 0, 0), 10, (+1, +1), scr)
+    bkd.update(scr)
 
     # 練習２
     while True:
-        scrn_sfc.blit(pgbg_sfc, pgbg_rct) 
+        scr.blit()
+
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
-        key_dct = pg.key.get_pressed()
-        for key, delta in key_delta.items():
-            if key_dct[key]:
-                tori_rct.centerx += delta[0]
-                tori_rct.centery += delta[1]
-            # 練習7
-            if check_bound(tori_rct, scrn_rct) != (+1, +1):
-                tori_rct.centerx -= delta[0]
-                tori_rct.centery -= delta[1]
-        scrn_sfc.blit(tori_sfc, tori_rct) # 練習3
-
-        # 練習６
-        bomb_rct.move_ip(vx, vy)
-        scrn_sfc.blit(bomb_sfc, bomb_rct) 
-        yoko, tate = check_bound(bomb_rct, scrn_rct)
-        vx *= yoko
-        vy *= tate
+        kkt.update(scr)
+        bkd.update(scr)
 
         # 練習８
-        if tori_rct.colliderect(bomb_rct):
+        if kkt.rct.colliderect(bkd.rct):
             return
 
         pg.display.update()
